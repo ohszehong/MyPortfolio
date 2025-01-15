ce3080f5b4194ba200a5c2a71dc8b0140df1e1bd
Oh Sze Hong <harryoh3831@gmail.com>
2025-01-15 15:20:38 +0800

    Setup color and font in configs

--- MyPortfolio/tailwind.config.js
+++ MyPortfolio/tailwind.config.js
@@ -6,6 +6,30 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'yellow': {
+        100: '#fffacd',
+        200: '#ffd700',
+      },
+      'purple': {
+        100: '#a67cba',
+        200: '#9370db',
+        300: '#6a5acd',
+        400: '#47399d',
+        500: '#8a2be2',
+        600: '#4b0082',
+      },
+      'pink': {
+        100: '#ffb6c1',
+        200: '#da70d6',
+      },
+      'bg-homepage': '#2f2f45',
+      'screen-color-gradient': {
+        'gradient1': '#7ea0f4',
+        'gradient2': '#eac4fa',
+      },
+      'console-button': '#d9d9d9',
+    },
   },
   plugins: [],
 }
