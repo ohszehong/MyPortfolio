1c47d528cac45c1a59413c6a107c9f1ba9570dbe
ohszehong <harryoh3831@gmail.com>
2025-02-19 15:39:49 +0800

    Add: Cassette.jsx

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import BgBlocks from './BgBlocks'
 import Console from './Console'
+import Cassette from './Cassette'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <p className='p-7'>Hi, Welcome to my portfolio.</p>
       <BgBlocks />
       <Console />
+      <Cassette />
     </>
   )
 }
---
+++ MyPortfolio/src/Cassette.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Cassette() {
+  return (
+    <div className='flex flex-nowrap justify-evenly'>
+    <div className='w-[150px] h-[50px] bg-purple-300'></div>
+    <div className='w-[150px] h-[50px] bg-pink-200'></div>
+    <div className='w-[150px] h-[50px] bg-yellow-100'></div>
+    <div className='w-[150px] h-[50px] bg-purple-100'></div>
+    </div>
+  )
+}
